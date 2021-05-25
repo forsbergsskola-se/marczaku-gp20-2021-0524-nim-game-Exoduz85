1f6e3e3e25374c070105385aeb7b43b4cf07bf26
Exoduz85 <47660094+Exoduz85@users.noreply.github.com>
2021-05-25 12:56:25 +0200

    adds loop and input from user and to user

--- nim/nim/nim.cpp
+++ nim/nim/nim.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 Player player;
-
+int sticks = 24;
+string sticksString = "|";
+int picketSticks = 0;
 
 int main()
 {
@@ -11,6 +13,22 @@ int main()
     string str = "";
     cin >> str;
     player.setName(str);
-    cout << "Let the game begin " + player.getName();
-    
+    cout << "Let the game begin " + player.getName() << endl;
+    for (;;) {
+        for (int i = 0; i < sticks; i++)
+        {
+            cout << sticksString;
+            if (i == sticks - 1) cout << endl;
+        };
+
+        cout << "How many sticks would you like to take? (1-3)" << endl;
+        string input = "";
+        cin >> input;
+        picketSticks = stoi(input);
+
+        sticks -= picketSticks;
+        
+        cout << "You took " << picketSticks << " sticks from the stack!" << endl << "Now " << sticks << " sticks remains in the stack!" << endl;
+    }
+    return 0;
 }
