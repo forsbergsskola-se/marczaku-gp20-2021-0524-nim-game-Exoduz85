34eba43ff1635f82e7e9c663c67a5d3fcc07f31e
Exoduz85 <47660094+Exoduz85@users.noreply.github.com>
2021-05-27 13:47:18 +0200

    adds functional tic tac toe
    needs to change checks to be after each players move.

--- tictactoe/tictactoe/tictactoe.cpp
+++ tictactoe/tictactoe/tictactoe.cpp
@@ -1,15 +1,136 @@
 #include <iostream>
 #include <vector>
+#include "ExtensionMethods.h"
 using namespace std;
 
-vector<vector<char>> tictactoe = {{'|', '|', '|'}, {'|', '|', '|'}, {'|', '|', '|'}};
+vector<vector<char>> board = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+
+string playerX = "";
+string playerO = "";
+string column = "";
+string row = "";
+int c = 0;
+int r = 0;
+int gameStatus = 3;
+
+void printBoard(){
+    cout << "\n\n\t" << board[0][2] << '|' << board[1][2] << '|' << board[2][2] <<  endl;
+    cout << "\t" << "-----" << endl;
+    cout << "\t" << board[0][1] << '|' << board[1][1] << '|' << board[2][1] <<  endl;
+    cout << "\t" << "-----" << endl;
+    cout << "\t" << board[0][0] << '|' << board[1][0] << '|' << board[2][0] << "\n\n";
+}
+
+int checkBoard(){
+	// 0 = tie
+    // 1 = player one wins
+    // 2 = player two wins
+    // 3 = Game continues
+	for(int i = 0; i < 3; i++){
+		if(board[i][0] == board[i][1] && board[i][1] == board[i][2]){
+			if(board[i][0] == 'X') return 1;
+			if(board[i][0] == 'O') return 2;
+		}
+		if(board[0][i] == board[1][i] && board[1][i] == board[2][i]){
+			if(board[0][i] == 'X') return 1;
+			if(board[0][i] == 'O') return 2;
+		}
+	}
+	if((board[0][0] == board[1][1] && board[1][1] == board[2][2]) || (board[0][2] == board[1][1] && board[1][1] == board[2][0])){
+		if(board[1][1] == 'X') return 1;
+		if(board[1][1] == 'O') return 2;
+	}
+	bool tieTest = true;
+	for(int i = 0; i < 3; i++)
+		for(int j = 0; j < 3; j++)
+			if(board[i][j] == ' ') tieTest = false;
+	if(tieTest) return 0;
+	return 3;
+}
+
+bool validateInput(const string & myFirstString, const string & mySecondString, int rangeStart, int rangeEnd){
+	if(!ExtensionMethods::IsIntOrNot(myFirstString) || !ExtensionMethods::IsIntOrNot(mySecondString)){
+		cout << "Input must be a number!\n";
+		return false;
+	}
+	int myFirstInt = stoi(myFirstString);
+	int mySecondInt = stoi(mySecondString);
+	if(!ExtensionMethods::IsInRange(myFirstInt, rangeStart, rangeEnd) || !ExtensionMethods::IsInRange(mySecondInt, rangeStart, rangeEnd)){
+		cout << "Input needs to be either 0, 1 or 2!\n";
+		return false;
+	}
+	if(board[myFirstInt][mySecondInt] != ' '){
+		cout << "You can not place on an already taken position\n";
+		return false;
+	}
+	return true;
+}
+
+void playerXMove(){
+	bool validInput = false;
+	while(!validInput)	{
+		cout << "Input column: ";
+		cin >> column;
+		cout << "\nInput row: ";
+		cin >> row;
+		if(!validateInput(column, row, 0, 2)) continue;
+		validInput = true;
+		c = stoi(column);
+		r = stoi(row);
+	}
+	board[c][r] = 'X';
+}
+
+void playerOMove(){
+	bool validInput = false;
+	while(!validInput){
+		cout << "Input column: ";
+		cin >> column;
+		cout << "\nInput row: ";
+		cin >> row;
+		if(!validateInput(column, row, 0, 2)) continue;
+		validInput = true;
+		c = stoi(column);
+		r = stoi(row);
+	}
+	board[c][r] = 'O';
+}
 
 int main()
 {
-    // print content of array
-    for(auto row : tictactoe){
-        for(auto col : row)
-            cout << col << " ";
-        cout << "\n";
-    }
+    cout << "Welcome to tic tac toe!\nPlease input a name for player X: ";
+    cin >> playerX;
+    cout << "\nPlease input a name for player O: ";
+    cin >> playerO;
+    cout << "\nLet the game begin :)\n";
+	printBoard();
+	cout << "Please select a column and row to place your marker (e.g Column: 2, Row: 2 for upper right corner)\n";
+	while(true)	{
+
+		// TODO, make checks after each players turn if the board has been filled, i.e we get another result than 3 from checkBoard()!
+
+		cout << playerX << "'s turn.\n";
+		playerXMove();
+		printBoard();
+		gameStatus = checkBoard();
+		cout << playerO << "'s turn.\n";
+		playerOMove();
+		printBoard();
+		gameStatus = checkBoard();
+		if(gameStatus == 3) continue;
+		if(gameStatus == 2){
+			cout << "Player O won the game.\nCongratulations!!!\n\n";
+			break;
+		}
+		if(gameStatus == 1){
+			cout << "Player X won the game.\nCongratulations!!!\n\n";
+			break;
+		}
+		if(gameStatus == 0){
+			cout << "Shoot!\nIt's a tie.\n\n";
+			break;
+		}
+	}
+	return 0;
 }
+
