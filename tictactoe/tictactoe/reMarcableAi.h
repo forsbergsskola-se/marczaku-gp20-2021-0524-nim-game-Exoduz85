2cc2149a1a9379773096f942f05d66058656ddb7
Exoduz85 <47660094+Exoduz85@users.noreply.github.com>
2021-05-28 09:32:44 +0200

    still stupid AI, but a little bit less stupid

--- tictactoe/tictactoe/reMarcableAi.h
+++ tictactoe/tictactoe/reMarcableAi.h
@@ -12,32 +12,32 @@ public:
 		// make AI check if any two spaces are occupied by double X's, if so place O in the right spot.
 		vector<int> result;
 		for(int i = 0; i < 3; i++){
-			if((myArray[i][0] && myArray[i][1]) == 'X' && myArray[i][2] == ' '){
+			if((myArray[i][0] == 'X' && myArray[i][1] == 'X')  && myArray[i][2] == ' '){
 				result.assign({i, 2});
 				return result;
 			}
-			if((myArray[i][2] == myArray[i][1]) == 'X' && myArray[i][0] == ' '){
+			if((myArray[i][2] == 'X' && myArray[i][1] == 'X')  && myArray[i][0] == ' '){
 				result.assign({i, 0});
 				return result;
 			}
-			if((myArray[2][i] == myArray[1][i]) == 'X' && myArray[0][i] == ' '){
+			if((myArray[2][i] == 'X' && myArray[1][i] == 'X') && myArray[0][i] == ' '){
 				result.assign({0, i});
 				return result;
 			}
-			if((myArray[0][i] == myArray[1][i]) == 'X' && myArray[2][i] == ' '){
+			if((myArray[0][i] == 'X' && myArray[1][i] == 'X') && myArray[2][i] == ' '){
 				result.assign({2, i});
 				return result;
 			}
-			if((myArray[2][i] == myArray[0][i]) == 'X' && myArray[1][i] == ' '){
+			if((myArray[2][i] == 'X' && myArray[0][i] == 'X') && myArray[1][i] == ' '){
 				result.assign({1, i});
 				return result;
 			}
-			if((myArray[i][0] == myArray[i][2]) == 'X' && myArray[i][1] == ' '){
+			if((myArray[i][0] == 'X' && myArray[i][2] == 'X') && myArray[i][1] == ' '){
 				result.assign({i, 1});
 				return result;
 			}
 			if(i == 0 || i == 2){
-				if(((myArray[2][0] == myArray[0][2]) == 'X' || (myArray[0][0] == myArray[2][2]) == 'X') && myArray[1][1] == ' '){
+				if(((myArray[2][0] == 'X' && myArray[0][2] == 'X') || (myArray[0][0] == 'X' && myArray[2][2] == 'X')) && myArray[1][1] == ' '){
 					result.assign({1, 1});
 					return result;
 				}
